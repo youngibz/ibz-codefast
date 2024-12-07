d36816ab8080028cfd27cc0c870a21bf718ff3cd
ibz <ibz.kamara@gmail.com>
2024-12-07 01:05:28 -0600

    Button commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Button</button>
+      <button className="btn">Get Started Button</button>
     </main>
   );
 }
