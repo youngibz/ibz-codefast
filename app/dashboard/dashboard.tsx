d504542a872de2c926a647bfa48af09cde824bd6
ibz <ibz.kamara@gmail.com>
2024-12-08 22:06:03 -0600

    Add dashboard page

---
+++ app/dashboard/dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+          <h1>User dashboard</h1>
+        </main>
+      );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started Button</button>
+      <h1>Collect customer feedback to build better products.</h1>
     </main>
   );
 }
